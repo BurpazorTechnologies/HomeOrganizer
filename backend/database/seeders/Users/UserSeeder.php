cccaa2f8f245ad2b82d3d63a2c420e03dbbc92aa
BurpazorTechnologies <burpazor@gmail.com>
2025-12-21 00:16:32 +0800

    updates: seeders

--- backend/database/seeders/Users/UserSeeder.php
+++ backend/database/seeders/Users/UserSeeder.php
@@ -10,13 +10,33 @@ use Spatie\Permission\Models\Role as RoleModel;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Default password for seeded users.
-     */
-    public const DEFAULT_PASSWORD = 'password123';
+    public const DEFAULT_PASSWORD = 'pass1234';
 
     public function run(): void
     {
+        $userList = [
+            [
+                'first_name' => 'Winzor',
+                'last_name' => 'Paelmo',
+                'email' => 'winzorjmpaelmo@gmail.com',
+            ],
+            [
+                'first_name' => 'Angelica',
+                'last_name' => 'Barcebal',
+                'email' => 'angelicambarcebal@gmail.com',
+            ],
+        ];
 
+        foreach ($userList as $user) {
+            User::query()->updateOrCreate(
+                ['email' => $user['email']],
+                [
+                    'first_name' => $user['first_name'],
+                    'last_name' => $user['last_name'],
+                    'password' => self::DEFAULT_PASSWORD,
+                    'email_verified_at' => now(),
+                ]
+            );
+        }
     }
 }
